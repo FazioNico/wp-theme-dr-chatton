6cf20c7561e833c96253fea7cdb95f9e924fc0a5
FazioNico <contact@nicolasfazio.ch>
2016-11-01 07:46:21 +0100

    theming single page..

--- single.php
+++ single.php
@@ -10,26 +10,31 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main  cours-single" role="main">
 
 		<?php
+		echo '<div class="row">';
+	  echo  '<div class="col col-sm-3 col-xs-12">';
+	  get_template_part( 'template-parts/aside', 'cours' );
+	  echo  '</div>';
+	  echo  '<div class="col col-sm-9 col-xs-12">';
+
 		while ( have_posts() ) : the_post();
-
 			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
+			//the_post_navigation();
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
 		endwhile; // End of the loop.
+
+	  echo  '</div>';
+	  echo '</div>';
 		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
